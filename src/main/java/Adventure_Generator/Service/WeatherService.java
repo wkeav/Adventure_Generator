950623d6e6359dbc3a2eab9415f807de843cd9ae
Astra <astra.k.nguyen05@outlook.com>
2025-06-19 15:29:05 -0400

    Working on log in

--- src/main/java/Adventure_Generator/Service/WeatherService.java
+++ src/main/java/Adventure_Generator/Service/WeatherService.java
@@ -1,5 +1,21 @@
 package Adventure_Generator.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import Adventure_Generator.API.WeatherApiClient;
+import Adventure_Generator.DTOs.Response.WeatherResponse;
+
+@Service
 public class WeatherService {
+    @Autowired
+    private WeatherApiClient weatherApiClient;
     
+    public WeatherResponse getCurrentWeather(double lat, double lon){
+        if(lat < -90 || lat > 90 || lon < -180 || lon > 180){
+            throw new IllegalArgumentException("Invalid coordinates, try again.");
+        }
+
+        
+    }
 }
