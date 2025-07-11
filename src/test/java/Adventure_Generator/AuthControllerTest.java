e83acfda2db9af3c869c9914c50421bf6b449a4f
Astra <astra.k.nguyen05@outlook.com>
2025-07-11 16:05:17 -0500

    wrote some manual and automated tests with the register/login endpoint for user authentication

---
+++ src/test/java/Adventure_Generator/AuthControllerTest.java
@@ -0,0 +1,42 @@
+package Adventure_Generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.springframework.http.MediaType;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthControllerTest {
+    
+    @Autowired
+    private MockMvc mockMvc;
+
+    // Register 
+    @Test 
+    public void testRegisterUser_Success() throws Exception{
+        String json = "{ \"email\": \"autotest1@example.com\", \"password\": \"pass1234\", \"confirmPassword\": \"pass1234\", \"userName\": \"AutoTest1\" }";
+        mockMvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.userData.email").value("autotest1@example.com"));
+    }
+
+    @Test
+    public void testRegisterUser_PasswordMismatch() throws Exception {
+        String json = "{ \"email\": \"autotest2@example.com\", \"password\": \"pass1234\", \"confirmPassword\": \"w12313\", \"userName\": \"AutoTest2\" }";
+        mockMvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false));
+    }
+
+}
