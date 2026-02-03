8106de8c75d307d7907caa7fccd234ae38fc8ed2
Astra <astra.k.nguyen05@outlook.com>
2026-02-03 17:03:57 -0600

    Update

--- src/main/java/Adventure_Generator/Entity/UserFavourite.java
+++ src/main/java/Adventure_Generator/Entity/UserFavourite.java
@@ -10,7 +10,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
